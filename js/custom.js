f61dcd61aa20c4ece9ba7df37c05a55fca94f115
Cosmas <ezejioforcosmas@gmail.com>
2018-11-20 07:02:01 +0100

    added animation

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,20 @@ $(".details-container p").text(function(index, currentText) {
     } 
   });
 
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
   // $(document).ready(function(){
   //   // Add smooth scrolling to all links
   //   $("a").on('click', function(event) {
